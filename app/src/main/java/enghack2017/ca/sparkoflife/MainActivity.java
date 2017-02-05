f364bba51ac49456ba9b66a8c6619fde8c4bdb4a
Shakaib Khan <shakaib97@gmail.com>
2017-02-04 19:37:04 -0500

    Added photon library

--- app/src/main/java/enghack2017/ca/sparkoflife/MainActivity.java
+++ app/src/main/java/enghack2017/ca/sparkoflife/MainActivity.java
@@ -1,9 +1,9 @@
 package enghack2017.ca.sparkoflife;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
